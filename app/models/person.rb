b98da47aef9c227fcbe02c9b959c7362c051cbbd
Natasha Natarajan <natashanatarajan@gmail.com>
2017-06-24 23:57:59 -0600

    validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,7 @@
 class Person < ApplicationRecord
+    validates :name, :age, :race, :hair_color, presence: true
+    validates :name, uniqueness:  true
+    #message: "%{value} already exists in the database"
+    validates :age, numericality: {only_integer: true,
+    message: "%{value} is not a valid age"}
 end
