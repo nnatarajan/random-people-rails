11c8418e4efbd05db31d3bc879bd6c82a2328208
Natasha Natarajan <natashanatarajan@gmail.com>
2017-06-25 23:02:43 -0600

    prevented negative age entered in db

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,6 +2,13 @@ class Person < ApplicationRecord
     validates :name, :age, :race, :hair_color, presence: true
     validates :name, uniqueness:  true
     #message: "%{value} already exists in the database"
-    validates :age, numericality: {only_integer: true,
-    message: "%{value} is not a valid age"}
+    validates :age, numericality: {greater_than_or_equal_to: 1} #{only_integer: true}
+    #validates :age, 
+    # validates :age_positive_integer
+
+    # def age_positive_integer
+    #     if age <= 0
+    #         errors.add(:age, "can't be a negative number")
+    #     end
+    # end
 end
